d0b18964aa8f3c88cd4c857b70fb833bcd1d2ff5
João Rafael <jrojoao@gmail.com>
2025-02-28 22:56:23 -0300

    responsive

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let currentColor = 'black';
 let screen = document.querySelector("#tela");
 let ctx = screen.getContext('2d');
-let canDraw = false 
+let canDraw = false;
 let mouseX = 0;
 let mouseY = 0;
 let isEraser = false;
@@ -9,60 +9,104 @@ let isEraser = false;
 
 document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent);
-})
+});
+
+
 document.querySelector("#eraser").addEventListener("click", () => {
     isEraser = true;
     currentColor = "white";
     document.querySelectorAll(".color").forEach(c => c.classList.remove("active"));
 });
-screen.addEventListener('mousedown',mousedownEvent);
-screen.addEventListener('mousemove',mouseMoveEvent);
-screen.addEventListener('mouseup', mouseUpEvent)
-document.querySelector('.clear').addEventListener('click',clearScreen);
+
+
+screen.addEventListener('mousedown', mousedownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
+
+
+screen.addEventListener('touchstart', touchStartEvent);
+screen.addEventListener('touchmove', touchMoveEvent);
+screen.addEventListener('touchend', touchEndEvent);
+
+
+document.querySelector('.clear').addEventListener('click', clearScreen);
+
+
 document.querySelector("#bucket").addEventListener("click", fillCanvas);
 
+
 function colorClickEvent(e) {
-    let color =  e.target.getAttribute('data-color');
+    let color = e.target.getAttribute('data-color');
     currentColor = color;
     isEraser = false;
-    document.querySelectorAll('.color .active').classList.remove('active');
-    e.target.classList.add('active')
-};
+    document.querySelectorAll('.color.active').forEach(c => c.classList.remove('active'));
+    e.target.classList.add('active');
+}
+
 
 function mousedownEvent(e) {
     canDraw = true;
     mouseX = e.pageX - screen.offsetLeft;
     mouseY = e.pageY - screen.offsetTop;
-};
-function mouseMoveEvent(e){
-    if (canDraw) {  
-        draw(e.pageX,e.pageY);
-    }
-};
-function mouseUpEvent() { 
-    canDraw = false;
-};
+}
 
-function draw(x,y) { 
+
+function mouseMoveEvent(e) {
+    if (canDraw) {
+        draw(e.pageX, e.pageY);
+    }
+}
+
+
+function mouseUpEvent() {
+    canDraw = false;
+}
+
+
+function touchStartEvent(e) {
+    canDraw = true;
+    let touch = e.touches[0];
+    mouseX = touch.pageX - screen.offsetLeft;
+    mouseY = touch.pageY - screen.offsetTop;
+    e.preventDefault(); 
+}
+
+
+function touchMoveEvent(e) {
+    if (canDraw) {
+        let touch = e.touches[0];
+        draw(touch.pageX, touch.pageY);
+        e.preventDefault(); 
+    }
+}
+
+
+function touchEndEvent() {
+    canDraw = false;
+}
+
+
+function draw(x, y) {
     let pointX = x - screen.offsetLeft;
     let pointY = y - screen.offsetTop;
 
     ctx.beginPath();
     ctx.lineWidth = 5;
     ctx.lineJoin = "round";
-    ctx.moveTo(mouseX,mouseY);
-    ctx.lineTo(pointX,pointY);
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
     ctx.closePath();
     ctx.strokeStyle = currentColor;
     ctx.stroke();
 
     mouseX = pointX;
-    mouseY = pointY
+    mouseY = pointY;
 }
-function clearScreen() {
-    ctx.setTransform(1,0,0,1,0,0);
-    ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
 
+
+function clearScreen() {
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
 
 
